eb73f18e9438e25aa50135889c2a0a042239e06c
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2020-05-20 21:02:05 +0300

    Initial setup and commit

---
+++ template/standard/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cout << 2;
+
+    for(int i = 0; i < 100; ++i) {
+        cout << i << endl;
+    }
+    
+    return 0;
+}
